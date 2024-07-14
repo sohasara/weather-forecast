0f92f82729a11663bbd1211ed80d56dc062d7919
NadiaIslamRupa <nadiaislamrupa853@gmail.com>
2024-07-14 14:32:48 +0600

    ymal change

--- lib/state_management/current_tem_state.dart
+++ lib/state_management/current_tem_state.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weather_forecast/api_service/current_weather/current_API_Service.dart';
 
 final apiserviceProvider = Provider<CurrentAPI>((ref) => CurrentAPI());
+// ignore: non_constant_identifier_names
 final WeatherProvider = FutureProvider((ref) {
   final apiService = ref.watch(apiserviceProvider);
   return apiService.fetchWeather();
