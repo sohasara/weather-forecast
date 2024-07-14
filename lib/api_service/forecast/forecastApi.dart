5e87984d65d4c04d78eb9c0e58a067537efef40c
NadiaIslamRupa <nadiaislamrupa853@gmail.com>
2024-07-14 21:22:06 +0600

    add current time

--- lib/api_service/forecast/forecastApi.dart
+++ lib/api_service/forecast/forecastApi.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: no_leading_underscores_for_local_identifiers, file_names
 
 import 'package:dio/dio.dart';
+import 'package:intl/intl.dart';
 import 'package:weather_forecast/api_service/forecast/forecast_data.dart';
 
 class ForeCastAPI {
@@ -16,11 +17,20 @@ class ForeCastAPI {
       final temperatures = List<double>.from(data['temperature_2m']);
 
       List<HourlyForecast> hourlyForecasts = [];
+      DateTime now = DateTime.now();
+      String todayDate = DateFormat('yyyy-MM-dd').format(now);
+
       for (int i = 0; i < times.length; i++) {
-        hourlyForecasts.add(HourlyForecast(
-          time: times[i],
-          temperature: temperatures[i],
-        ));
+        DateTime forecastDate = DateTime.parse(times[i]);
+        String forecastDateString =
+            DateFormat('yyyy-MM-dd').format(forecastDate);
+
+        if (forecastDateString == todayDate) {
+          hourlyForecasts.add(HourlyForecast(
+            time: times[i],
+            temperature: temperatures[i],
+          ));
+        }
       }
 
       return hourlyForecasts;
